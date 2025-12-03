baabfc0ec51d4ecc8ffd701d3cbbd546f11b031e
Alisher Muhammadaliyev <alishermuhammadaliyev2508@gmail.com>
2025-12-03 23:11:15 +0500

    user section starting

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(("first_name"), max_length=50)
+    last_name = models.models.CharField(("last_name"), max_length=50)
+    profile_picture = models.ImageField()
+    
